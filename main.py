10007ff0353966c22c471767f47d06a1cbd18426
tobiasfrahm <frahm.tobias@gmail.com>
2021-07-10 16:35:41 +0200

    chore: improvement of RSA and EC, clean up, add prints

--- main.py
+++ main.py
@@ -16,24 +16,34 @@ def enable_print():
     sys.stdout = sys.__stdout__
 
 
-def greatest_common_divisor(a, b):
+def greatest_common_divisor(a, b, out=True):
     # ggT(a, b)
-    # print(f'Calculating the greatest common divisor of {a}, {b} ...')
+    print(f'Calculating the greatest common divisor of {a}, {b} ...')
+    if not out:
+        block_print()
     r = 0
     if a == 0:
         return abs(b)
     if b == 0:
         return abs(a)
+    print(f'{b} = {int(b / a)} * {a} + {b - a * int(b / a)}')
     while b != 0:
         r = a % b
         a = b
         b = r
-    # print(f'Greatest common divisor: {abs(a)}')
+        try:
+            print(f'{b} = {int(a / b) } * {b} + {a % b}')
+        except BaseException as err:
+            pass
+    if not out:
+        enable_print()
+    print(f'Greatest common divisor: {abs(a)}')
     return abs(a)
 
 
 def is_not_comparable(a, b):
     # print(f'Check if {a}, {b} are coprime to each other...')
+
     if greatest_common_divisor(a, b) == 1:
         # print(f'{a}, {b} are coprime to each other')
         return True
@@ -46,6 +56,7 @@ def eulersche_phi(n):
     body = []
     phi = 0
     # print(f'Calculate the Body for n={n}')
+    block_print()
     for i in range(n):
         if is_not_comparable(i, n):
             # phi(n) is the number of coprime numbers within the body of n
@@ -53,6 +64,7 @@ def eulersche_phi(n):
             # phi(i * n) = phi(i) * phi(n), if ggT(i,n) = 1
             phi += 1
         body.append(i + 1)
+    enable_print()
     print(f'phi({n}) = {phi}')
     return phi
 
@@ -92,7 +104,8 @@ def extended_euclidean_algorithm(a, b):
         ymitte = yneu
         aalt = amitte
         amitte = aneu
-        print(f'{amitte} = {xmitte} * {a} + {ymitte} * {b}')
+        if amitte != 0:
+            print(f'{amitte} = {xmitte} * {a} + {ymitte} * {b}')
     return (aalt, xalt, yalt)
 
 
@@ -109,6 +122,15 @@ def mod_inv(a, m):
 
 
 def multiplicative_inverse_modulo(a, m, out=False):
+    """
+    m = n * a + rest
+    n: is calculated
+    rest: is calculated
+    :param a:
+    :param m:
+    :param out:
+    :return:
+    """
     # modulare multiplikative inverse
     _a = a
     _m = m
@@ -116,7 +138,7 @@ def multiplicative_inverse_modulo(a, m, out=False):
     equations = []
     if not out:
         block_print()
-    if greatest_common_divisor(a, m) == 1:
+    if greatest_common_divisor(a, m, out=False) == 1:
         while r != 0:
             p = m // a
             r = m - (a * p)
@@ -154,39 +176,57 @@ def elliptic_curve(P, Q, a, p):
     s = 0
     (x_1, y_1) = (P[0], P[1])
     (x_2, y_2) = (Q[0], Q[1])
-    #print(P, Q)
+    # print(P, Q)
     if P != Q:
-     #   print('Unequal, Punktaddition')
         p1 = (y_2 - y_1)
-        p2 = multiplicative_inverse_modulo((x_2 - x_1), p)
+        p2 = multiplicative_inverse_modulo((x_2 - x_1), p, out=True)
         s = (p1 * p2) % p
+        print('Unequal, Punktaddition')
+        print(f's = ((y2 - y1) * (x2 - x1)⁻¹) mod p')
+        print(f's = ({y_2} - {y_1} * ({x_2} - {x_1})⁻¹) mod {p} = {s}')
     else:
-      #  print('Equal, Verdopplung')
         p1 = (3 * pow(x_1, 2) + a)
-        p2 = multiplicative_inverse_modulo((2 * y_1), p)
+        p2 = multiplicative_inverse_modulo((2 * y_1), p, out=True)
         s = (p1 * p2) % p
-
+        print('Equal, Verdopplung')
+        print(f's = ((3 * x1² + a) * (2 * y1)⁻¹) mod p')
+        print(f's = (3 * {x_1}² + {a} * (2*{y_1})⁻¹) mod {p} = {s}')
     x_3 = (pow(s, 2) - x_1 - x_2)
     x_3 = x_3 % p
     y_3 = (s * (x_1 - x_3) - y_1)
     y_3 = y_3 % p
+    print(f'x3 = (s² - x1 - x2) mod p = ({pow(s, 2)} - {x_1} - {x_2}) mod {p} = {x_3}')
+    print(f'y3 = (s (x1 - x3) - y1) mod p = ({s} ({x_1} - {x_3}) - {y_1}) mod {p} = {y_3}')
 
     return x_3, y_3
 
 
-def diffie_hellman_RSA(p, alpha):
+def diffie_hellman(p, alpha, alice=0, bob=0):
+    """
+    :param p: modulo num
+    :param alpha:
+    :param alice: alices private key
+    :param bob: bobs private key
+    :return:
+    """
     if diffie_hellman_check(p, alpha):
         print(f'[Public]Selected Domain Parameter: p = {p}, alpha = {alpha}')
 
-    a = random.randint(2, (p - 2))
+    if not alice:
+        a = random.randint(2, (p - 2))
+    else:
+        a = alice
     print(f'[Alice (private)] a = k_pr,A = {a}')
-    b = random.randint(2, (p - 2))
+    if not bob:
+        b = random.randint(2, (p - 2))
+    else:
+        b = bob
     print(f'[Bob (private)] b = k_pr,B = {b}')
 
     A = pow(alpha, a) % p
     B = pow(alpha, b) % p
-    print(f'A = k_pub,A = alpha^a = {A}')
-    print(f'B = k_pub,B = alpha^b = {B}')
+    print(f'A = k_pub,A = alpha^a mod p = {alpha}^{a} mod {p} = {A}')
+    print(f'B = k_pub,B = alpha^b mod p = {alpha}^{b} mod {p} = {B}')
     print(f'Alice --- {A} ----> Bob')
     print(f'Alice <---- {B} --- Bob')
 
@@ -198,8 +238,8 @@ def diffie_hellman_EC(p, P, alice, bob, a):
     """
     :param p: modulo param, prime number
     :param P: Point (x1, y1)
-    :param alice: How often will Alice Calculate the Curve
-    :param bob: How often will Bob Calculate the Curve
+    :param alice: How often will Alice Calculate the Curve (private Key Alice)
+    :param bob: How often will Bob Calculate the Curve (private Key Bob)
     :param a: factor from y² = x³ + a * x + b mod p
     :return:
     """
@@ -208,12 +248,15 @@ def diffie_hellman_EC(p, P, alice, bob, a):
     print('------------------------------------')
     print(f'Alice chooses random Number a = {alice}')
     print(f'Calculate A = k_pubA {alice}{P}')
-    for i in range(alice - 1):
-        P_New = elliptic_curve((P_New if 'P_New' in locals() else P_Base), P, a, p)
-        if not i:
-            print(f'{i + 2}P = {P} + {P}')
-        else:
-            print(f'{i + 2}P = {P_New} + {P}')
+    if alice == 1:
+        P_New = P
+    else:
+        for i in range(alice - 1):
+            P_New = elliptic_curve((P_New if 'P_New' in locals() else P_Base), P, a, p)
+            if not i:
+                print(f'{i + 2}P = {P} + {P}')
+            else:
+                print(f'{i + 2}P = {P_New} + {P}')
     k_pub_alice = P_New
 
     # reset
@@ -221,26 +264,34 @@ def diffie_hellman_EC(p, P, alice, bob, a):
     print('------------------------------------')
     print(f'Bob chooses random Number b = {bob}')
     print(f'Calculate B = k_pubB {bob}{P}')
-
-    for i in range(bob - 1):
-        P_New = elliptic_curve((P_New if 'P_New' in locals() else P_Base), P, a, p)
-        if not i:
-            print(f'{i + 2}P = {P} + {P}')
-        else:
-            print(f'{i + 2}P = {P_New} + {P}')
+    if bob == 1:
+        P_New = P
+    else:
+        for i in range((bob - 1) if bob > 1 else 1):
+            P_New = elliptic_curve((P_New if 'P_New' in locals() else P_Base), P, a, p)
+            if not i:
+                print(f'{i + 2}P = {P} + {P}')
+            else:
+                print(f'{i + 2}P = {P_New} + {P}')
     k_pub_bob = P_New
     print(f'\nPublic Keys:\n----------- \nAlice: A = {k_pub_alice} \nBob: B = {k_pub_bob}\n-----------')
     print('Take the Public Key, Calc Private Key')
 
     # reset
-    #Tab = alice*B
+    # Tab = alice*B
     del P_New
+
     P_Base = (k_pub_bob if alice < bob else k_pub_alice)
-    for i in range((alice if alice < bob else bob) - 1):
-        P_New = elliptic_curve((P_New if 'P_New' in locals() else P_Base), P_Base, a, p)
+    tmp = (alice if alice < bob else bob)
+    if tmp == 1:
+        P_New = P_Base
+    else:
+        for i in range((tmp - 1) if tmp > 1 else 1):
+            P_New = elliptic_curve((P_New if 'P_New' in locals() else P_Base), P_Base, a, p)
 
     k_priv_alice_bob = P_New
 
+    print(f'b * KpubA = a * KpubB = {bob} * {k_pub_alice} = {alice} * {k_pub_bob}')
     print(f'Private Key for Alice AND Bob: {k_priv_alice_bob}')
 
 
@@ -265,7 +316,7 @@ def RSA(p, q, e=0):
                     break
 
     print('d is a number: 1 < d < phi(n) and e*d = 1 mod phi(n)')
-    d = multiplicative_inverse_modulo(e, phi)
+    d = multiplicative_inverse_modulo(e, phi, out=True)
     if d > 0:
         print(f'd = {d}')
         print('----------------------------------')
@@ -280,6 +331,13 @@ def RSA(p, q, e=0):
 
 
 def message_RSA(num, p, q, e=0):
+    """
+    :param num: Message
+    :param p: p
+    :param q: q
+    :param e: e
+    :return:
+    """
     # block_print()
     start = time.time_ns()
     if e != 0:
@@ -295,8 +353,10 @@ def message_RSA(num, p, q, e=0):
     end = time.time_ns()
     # enable_print()
     print(f'Message: {num}')
-    print(f'Encrypted {chiffre}')
-    print(f'Decrypted {cleartext}')
+    print(f'Encrypted: {num}exp({e}) mod {n} = {chiffre}')
+    print(f'Decrypted: {chiffre}exp({private}) mod {n} = {cleartext}')
+    print(f'Signatur: {num}exp({private}) mod {n} = {pow(num, private) % n}')
+    print(f'Verify Signatur: {pow(num, private) % n}exp({e}) mod {n} = {pow(pow(num, private) % n, e) % n}')
     print(f'Total time: {round(round(end - start) / 1000000, 2)} ms')
 
 
@@ -305,11 +365,15 @@ def hex_to_bin(hexStr):
 
 
 if __name__ == '__main__':
-    # message_RSA(10, 61, 97, 47)
-    # print(elliptic_curve((3, 8), (3, 8), (-43), 11))
-    # multiplicative_inverse_modulo(2, 11)
+    #### RSA ####
+    # message_RSA(4, 13, 7)
     # RSA(5, 11, 7)
-    # diffie_hellman_EC(11, (3, 8), 2, 10, (-43))
-    # greatest_common_divisor(13, 7)
+
+    #### Elliptische Kurve ####
+    # diffie_hellman_EC(11, (2, 1), 2, 1, 1)
+
+    #### MISC ####
+    # print(greatest_common_divisor(2689, 4001))
+    # multiplicative_inverse_modulo(2, 11)
     # fermats_little_theorem(7, 26, 53)
-    # print(hex_to_bin('F0F'))
+    print('Done...')
